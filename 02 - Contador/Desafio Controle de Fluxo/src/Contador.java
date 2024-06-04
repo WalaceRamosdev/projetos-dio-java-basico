33546f0eeb2544be7041be52d575e2252a7fe0d1
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-04 03:38:51 -0300

    finalizado

--- 02 - Contador/Desafio Controle de Fluxo/src/Contador.java
+++ 02 - Contador/Desafio Controle de Fluxo/src/Contador.java
@@ -1,5 +1,4 @@
 import java.util.InputMismatchException;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Contador {
@@ -14,27 +13,24 @@ public class Contador {
 		int parametroDois = terminal.nextInt();
 		
 		try {
-			//chamando o método contendo a lógica de contagem
+			
 			contar(parametroUm, parametroDois);
 
-            if (parametroUm < parametroDois) {  
-                int contar = parametroDois - parametroUm;
-                for(int contar = )
-            }
-		
 		}catch (InputMismatchException e) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			
             System.out.println("O segundo número selecionado dever ser maior que o primeiro.");
 		}
 		
 	}
+
 	static void contar(int parametroUm, int parametroDois ) throws InputMismatchException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
 		if(parametroUm > parametroDois) {
-            
+            throw new InputMismatchException("O segundo parametro deve ser maior que o primeiro.");
         }
-		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
-        for(int i = contagem; contagem < parametroUm; i++ );
+		
+        for(int count = parametroUm; count <= parametroDois; count++ ){
+            System.out.println("Imprimindo número " + count);
+        }
 	}
 }
