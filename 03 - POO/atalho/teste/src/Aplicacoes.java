54c7986f95a992ae65256492c43655899ed54747
Walace Ramos <contatowalace.dev@gmail.com>
2024-06-22 11:33:32 -0300

    iPhone

---
+++ 03 - POO/atalho/teste/src/Aplicacoes.java
@@ -0,0 +1,12 @@
+public class Aplicacoes {
+    public void ligar() {
+        System.out.println("Realizando uma ligacao");
+        System.out.println("Iniciando correio de voz");
+        System.out.println("Atendendo uma ligacao");
+    }
+
+    public void reprodutorMusical () {
+        System.out.println("");
+    }
+
+}
